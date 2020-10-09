25c8b19981a13798f30cbe535bcb59a89adc7a18
mintqaq <mintqaq@gmail.com>
2020-10-09 17:47:49 +0800

    add base info

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ def get_content(target):
     return content
 
 if __name__ == '__main__':
-    # server = (input("add a server html:"))
-    inputKeyword = (input("Search Name:"))
+    # search book name
+    inputKeyword = (input("输入书名:"))
+    # server base URl
     searchUrl = "https://www.xsbiquge.com/search.php"
     header = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
@@ -23,7 +24,7 @@ if __name__ == '__main__':
     searchParams = {
         'keyword' :inputKeyword
     }
-
+    # request then filter useful data
     req = requests.get(searchUrl, headers=header, params=searchParams)
     req.encoding = "utf-8"
     html = req.text
@@ -31,28 +32,30 @@ if __name__ == '__main__':
     bookListInfo = bs.find_all('a', 'result-game-item-title-link')
     bookInfo = []
     count = 0
+    # append new array get book name and download url
     for list in bookListInfo:
         bookInfo.append([count, list['title'], list['href']])
         count+=1
     for formatList in bookInfo:
         print(formatList[0],'.',formatList[1])
-    inputBookNumber = int((input("Select:")))
-    bookName = bookInfo[inputBookNumber][1]
+    # select book
+    inputBookNumber = int((input("输入序号:")))
+    # combine into a file name
+    bookName = bookInfo[inputBookNumber][1]+'.txt'
     selectBookUrl = bookInfo[inputBookNumber][2]
-    # target = (input("Add a target url(like :https://www.xsbiquge.com/15_15338):"))
+    # base server url
     server = 'https://www.xsbiquge.com'
-    # target = 'https://www.xsbiquge.com/15_15338'
-    # book_name = 'book.txt'
     req = requests.get(url=selectBookUrl)
     req.encoding = 'utf-8'
     html = req.text
     char_bs = BeautifulSoup(html, 'lxml')
+    # filter
     chapters = char_bs.find('div', id='list')
     chapters = char_bs.find_all('a')
     for chapter in tqdm(chapters):
         chapter_name = chapter.string
         url = server+chapter.get('href')
-
+        # remove useless chapter,only keep chapter name like '章'
         if  '章' in chapter_name:
             content = get_content(url)
             with open(bookName, 'a', encoding='utf-8') as f:
